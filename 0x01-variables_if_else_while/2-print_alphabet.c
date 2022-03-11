a860a1630dbc2ec2cf05f1dbd9cf584860fd178e
ambanigodwin <ambanigodwin29@gmail.com>
2022-03-12 00:39:09 +0300

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int main (void)
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
 {
-	char ch = 'a';
-	while (ch <= 'z'):
-		{
-			putchar ("%c", ch);
-			ch ++;
-		}
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+		putchar('\n');
+
 	return (0);
 }	
